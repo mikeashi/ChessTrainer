f37164bcbb01065dd9eeba5761361a898d7e4116
Mike Ashi <mikeashi2013@gmail.com>
2020-04-20 04:16:16 +0200

    get starting position from server

--- static/app/src/app.ts
+++ static/app/src/app.ts
@@ -1,20 +1,33 @@
+const $ = require( "jquery" );
 const Chessground = require('chessground').Chessground;
 const Chess = require('chess.js').Chess;
-
-
 const chess = new Chess();
 
-function setZoom(zoom: number) {
-    const el = document.querySelector('.cg-wrap') as HTMLElement;
-    if (el) {
-        const px = `${zoom / 100 * 320}px`;
-        el.style.width = px;
-        el.style.height = px;
-        document.body.dispatchEvent(new Event('chessground.resize'));
-    }
-}
+/**
+ * return starting fen.
+ */
+function getStartPositionFen():string {
+    let fen = null;
 
+    $.ajax({
+        async: false,
+        type: 'GET',
+        url: "/get_start",
+        success: function (data:string) {
+             fen = data
+        }
+    });
+
+    console.log('fen:' + fen);
+
+    return fen;
+}
+/**
+ * return available moves
+ * @param chess
+ */
 function toDests(chess: any) {
+    console.log('all the moves are comming from here !!')
     const dests = {};
     // @ts-ignore
     chess.SQUARES.forEach(s => {
@@ -24,11 +37,37 @@ function toDests(chess: any) {
             dests[s] = ms.map(m => m.to);
         }
     });
+    console.log('moves:');
+    console.log(dests);
     return dests;
+}
+
+/**
+ * return the player color
+ * @param chess
+ */
+function toColor(chess: any) {
+    return (chess.turn() === 'w') ? 'white' : 'black';
+}
+
+
+// @ts-ignore
+function playOtherSide(board, chess) {
+    return (orig: any, dest: any) => {
+        chess.move({from: orig, to: dest});
+        board.set({
+            turnColor: toColor(chess),
+            movable: {
+                color: toColor(chess),
+                dests: toDests(chess)
+            }
+        });
+    };
 }
 
 
 const config = {
+    fen: getStartPositionFen(),
     movable: {
         color: 'white',
         free: false,
@@ -41,4 +80,9 @@ const config = {
 
 const board = Chessground(document.getElementById('chessboard'), config);
 
-//setZoom(200);
+// @ts-ignore
+window['board'] = board;
+
+board.set({
+    movable: {events: {after: playOtherSide(board, chess)}}
+});
